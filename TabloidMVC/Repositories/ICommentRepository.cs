8f0581367712e8295762c49eb5ad22f602c17f8a
Samuel <samuelcatferrell@gmail.com>
2022-01-06 10:19:36 -0800

    Added ability to edit comments

--- TabloidMVC/Repositories/ICommentRepository.cs
+++ TabloidMVC/Repositories/ICommentRepository.cs
@@ -6,6 +6,7 @@ namespace TabloidMVC.Repositories
     public interface ICommentRepository
     {
         List<Comment> GetAllPostComments(int postId);
+        public Comment GetCommentById(int postId, int id);
         public List<Comment> GetAllPostComments(int postId, IPostRepository repo);
         void AddComment(Comment comment);
         void UpdateComment(Comment comment);
